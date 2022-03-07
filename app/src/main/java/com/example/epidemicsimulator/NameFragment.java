3ba3659f59fb16aabcab0274c0259fb43ea1d3b3
Yiyang-Qiu <qiu.464@osu.edu>
2022-03-06 20:22:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/epidemicsimulator/NameFragment.java
@@ -0,0 +1,179 @@
+package com.example.epidemicsimulator;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NameFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NameFragment extends Fragment {
+
+    private static final String TAG = "NameFragment";
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private String mName;
+    private EditText mNameField;
+    private Button mCheckButton;
+    private SharedPreferences mNameSharedPref;
+
+    public NameFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NameFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NameFragment newInstance(String param1, String param2) {
+        NameFragment fragment = new NameFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Log.d(TAG, "onCreateView(LayoutInflater, ViewGroup, Bundle) called");
+
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_name,container,false);
+        mNameField=(EditText) v.findViewById(R.id.input_box);
+        mNameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // temporarily leave it blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // set name
+                mName = charSequence.toString();
+                if(mName.length()!=0){
+                    mCheckButton.setEnabled(true);
+                }
+                else{
+                    mCheckButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // temporarily leave it blank
+            }
+        });
+
+
+
+
+        mCheckButton=(Button)v.findViewById(R.id.check_button);
+        mCheckButton.setEnabled(false);   // Initially disabled to prevent empty input
+        mCheckButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mNameSharedPref= getActivity().getSharedPreferences("nameData", Context.MODE_PRIVATE);
+                String temp = mNameSharedPref.getString(mName,"NAME_NOT_EXIST");
+                if (temp.equals("NAME_NOT_EXIST")){
+                    // name haven't been used before, store it
+                    mNameSharedPref.edit().putString(mName,"PLACE_HOLDER").apply();   // value doesn't matter here
+                    Toast.makeText(getActivity(),"Add successfully",Toast.LENGTH_SHORT).show();
+                    // TO DO: go to next page!
+
+
+                }else{
+                    Toast.makeText(getActivity(),"This name has been used!",Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }
+        });
+
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView() called");
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+
+    }
+}
